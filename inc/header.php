adb3909f7ee40aa34b0495eb58218891425a170e
quoctuan-sgu <thotrangpro2@gmail.com>
2024-03-06 00:47:55 +0700

    update FE and cut PHP

---
+++ inc/header.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Product</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.png" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/linearicons-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/MagnificPopup/magnific-popup.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+</head>
+
+<body class="animsition">
+
+    <!-- Header -->
+    <header>
+        <!-- Header desktop -->
+        <div class="container-menu-desktop">
+            <!-- Topbar -->
+            <div class="top-bar">
+                <div class="content-topbar flex-sb-m h-full container">
+                    <div class="left-top-bar">
+                        Free shipping cho đơn hàng trên 500.000đ
+                    </div>
+
+                    <div class="right-top-bar flex-w h-full">
+                        <a href="/signin.php" class="flex-c-m trans-04 p-lr-25">Đăng nhập</a>
+
+                        <a href="#" class="flex-c-m trans-04 p-lr-25">Đơn hàng</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wrap-menu-desktop">
+                <nav class="limiter-menu-desktop container">
+
+                    <!-- Logo desktop -->
+                    <a href="#" class="logo">
+                        <img src="images/icons/logo-01.png" alt="IMG-LOGO">
+                    </a>
+
+                    <!-- Menu desktop -->
+                    <div class="menu-desktop">
+                        <ul class="main-menu">
+                            <li>
+                                <a href="index.php">Home</a>
+                            </li>
+
+                            <li>
+                                <a href="cart.php">Cart</a>
+                            </li>
+
+                            <li class="label1" data-label1="hot">
+                                <a href="product.php">Shop</a>
+                            </li>
+
+                            <li>
+                                <a href="blog.php">Blog</a>
+                            </li>
+
+                            <li>
+                                <a href="about.php">About</a>
+                            </li>
+
+                            <li>
+                                <a href="contact.php">Contact</a>
+                            </li>
+                        </ul>
+
+                        <script>
+                            // Lấy đường dẫn hiện tại
+                            var currentLocation = window.location.pathname;
+                            // Lấy tất cả các thẻ <a> trong menu
+                            var menuItems = document.querySelectorAll('.main-menu li a');
+
+                            menuItems.forEach(function(item) {
+                                if (currentLocation.includes(item.getAttribute('href'))) {
+                                    item.parentElement.classList.add('active-menu');
+                                }
+                            });
+                        </script>
+                    </div>
+
+                    <!-- Icon header -->
+                    <div class="wrap-icon-header flex-w flex-r-m">
+                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="99">
+                            <i class="zmdi zmdi-shopping-cart"></i>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <!-- Header Mobile -->
+        <div class="wrap-header-mobile">
+            <!-- Logo moblie -->
+            <div class="logo-mobile">
+                <a href="index.php"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></a>
+            </div>
+
+            <!-- Icon header -->
+            <div class="wrap-icon-header flex-w flex-r-m m-r-15">
+                <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="2">
+                    <i class="zmdi zmdi-shopping-cart"></i>
+                </div>
+            </div>
+
+            <!-- Button show menu -->
+            <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </div>
+        </div>
+
+
+        <!-- Menu Mobile -->
+        <div class="menu-mobile">
+            <ul class="topbar-mobile">
+                <li>
+                    <div class="left-top-bar">
+                        Free shipping cho đơn hàng trên 500.000đ
+                    </div>
+                </li>
+
+                <li>
+                    <div class="right-top-bar flex-w h-full">
+                        <a href="#" class="flex-c-m p-lr-10 trans-04">
+                            Đăng nhập
+                        </a>
+
+                        <a href="#" class="flex-c-m p-lr-10 trans-04">
+                            Đơn hàng
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+            <ul class="main-menu-m">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+
+                <li>
+                    <a href="product.php" class="label1 rs1" data-label1="hot">Shop</a>
+                </li>
+
+                <li>
+                    <a href="shoping-cart.php">Cart</a>
+                </li>
+
+                <li>
+                    <a href="blog.php">Blog</a>
+                </li>
+
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </header>
