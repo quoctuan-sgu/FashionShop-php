a8e0264db7d25f67070df7c51c354e9c0ffcab66
dante <dantetran04@gmail.com>
2024-05-17 23:24:36 +0700

    fix admin

--- admin/category/delete.php
+++ admin/category/delete.php
@@ -25,7 +25,17 @@
                 <label for="category_id">Category ID</label>
                 <input type="text" class="form-control" id="category_id" name="category_id" value=<?php echo $category['category_id'] ?> hidden>
                 <input type="text" class="form-control" id="category_id" name="category_id_show" value=<?php echo $category['category_id'] ?> disabled>
-                <button type="submit" class="btn btn-primary mt-2">Delete</button>
+                
+                <?php if(isCategoryContainProduct($category_id)) {?>
+                    <div class="alert alert-danger mt-2" role="alert">
+                        This category contains product. You can not delete this category.
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-2" disabled>Delete</button>
+
+                    <?php } ?>
+                    <?php if(!isCategoryContainProduct($category_id)){?>
+                        <button type="submit" class="btn btn-primary mt-2">Delete</button>
+                    <?php }?>
             </div>
         </form>
         <a href="index.php?ac=category" class="btn btn-secondary">Go Back</a>
