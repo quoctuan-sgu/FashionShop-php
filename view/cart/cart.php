998ba3c09724a15c1567293db9b6b3a32f6263a8
Nguyen Van Dung <vandungng1101@gmail.com>
2024-05-17 22:23:58 +0700

    fix code

--- view/cart/cart.php
+++ view/cart/cart.php
@@ -253,7 +253,7 @@
 						</table>
 					</div>
 
-					<div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
+					<!-- <div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
 						<div class="flex-w flex-m m-r-20 m-tb-5">
 							<input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text" name="coupon" placeholder="Coupon Code">
 
@@ -265,7 +265,7 @@
 						<div class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
 							<button type="submit" name="btn_update_cart">Update Cart</button>
 						</div>
-					</div>
+					</div> -->
 
 
 				</div>
